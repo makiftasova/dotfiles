53e718e894dbdf97f6cc703a1ac67ecacdc44863
Mehmet Akif Tasova <makiftasova@gmail.com>
2019-10-03 14:47:16 +0300

    update sway_bar.sh
    * update pamixer commands
    * use systemd's "new" network interafce names
    * define variables for each emoji
    * split week and date parts of date_and_week
    * cosmetic changes (make commands multiline etc.)
    * reduce duplicate calls as much as possible
    * disable unused fields
    * add pamixer and playerctl to pacman install list

--- config/sway/sway_bar.sh
+++ config/sway/sway_bar.sh
@@ -7,7 +7,8 @@
 keyboard_input_name="1:1:AT_Translated_Set_2_keyboard"
 
 # Date and time
-date_and_week=$(date "+%Y/%m/%d (w%-V)")
+date=$(date "+%Y/%m/%d")
+# week=$(date "+w%-V")
 current_time=$(date "+%H:%M")
 
 #############
@@ -15,13 +16,16 @@ current_time=$(date "+%H:%M")
 #############
 
 # Battery or charger
-battery_charge=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "percentage" | awk '{print $2}')
-battery_status=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "state" | awk '{print $2}')
-battery_acpi=$(acpi -V | egrep -i "charging" | awk '{print $3  $4  $5}' | tr ',' ' ')
+battery=$(upower --enumerate | grep 'BAT')
+battery_charge=$(upower --show-info $battery | egrep "percentage" | awk '{print $2}')
+battery_status=$(upower --show-info $battery | egrep "state" | awk '{print $2}')
+battery_upower=$(upower --show-info $battery | egrep "time to" | awk '{print $4}')
+# battery_acpi=$(acpi -V | egrep -i "charging" | awk '{print $3  $4  $5}' | tr ',' ' ')
 
 # Audio and multimedia
-audio_volume=$(pamixer --sink `pactl list sinks short | grep RUNNING | awk '{print $1}'` --get-volume)
-audio_is_muted=$(pamixer --sink `pactl list sinks short | grep RUNNING | awk '{print $1}'` --get-mute)
+audio_sink=$(pactl list sinks short | awk '{print $1}')
+audio_volume=$(pamixer --sink  $audio_sink --get-volume)
+audio_is_muted=$(pamixer --sink $audio_sink --get-mute)
 media_artist=$(playerctl metadata artist)
 media_song=$(playerctl metadata title)
 player_status=$(playerctl status)
@@ -29,16 +33,19 @@ player_status=$(playerctl status)
 # Network
 network=$(ip route get 1.1.1.1 | grep -Po '(?<=dev\s)\w+' | cut -f1 -d ' ')
 # interface_easyname grabs the "old" interface name before systemd renamed it
-interface_easyname=$(dmesg | grep $network | grep renamed | awk 'NF>1{print $NF}')
-ping=$(ping -c 1 www.google.es | tail -1| awk '{print $4}' | cut -d '/' -f 2 | cut -d '.' -f 1)
+# interface_easyname=$(dmesg | grep $network | grep renamed | awk 'NF>1{print $NF}')
+network_ip=$(ip addr show dev $network | grep -Po '(?<=inet\s)(\w+\.)+\w+')
+#ping=$(ping -c 1 www.archlinux.org | tail -1| awk '{print $4}' | cut -d '/' -f 2 | cut -d '.' -f 1)
 
 # Others
-language=$(swaymsg -r -t get_inputs | awk '/1:1:AT_Translated_Set_2_keyboard/;/xkb_active_layout_name/' | grep -A1 '\b1:1:AT_Translated_Set_2_keyboard\b' | grep "xkb_active_layout_name" | awk -F '"' '{print $4}')
-loadavg_5min=$(cat /proc/loadavg | awk -F ' ' '{print $2}')
+language=$(swaymsg -r -t get_inputs |\
+	awk '/1:1:AT_Translated_Set_2_keyboard/;/xkb_active_layout_name/' |\
+	grep -A1 '\b1:1:AT_Translated_Set_2_keyboard\b' |\
+	grep "xkb_active_layout_name" |\
+	awk -F '"' '{print $4}')
 
-# Removed weather because we are requesting it too many times to have a proper
-# refresh on the bar
-#weather=$(curl -Ss 'https://wttr.in/Pontevedra?0&T&Q&format=1')
+loadavg=$(cat /proc/loadavg | awk -F ' ' '{print $1" "$2" "$3}')
+#loadavg_5min=$(cat /proc/loadavg | awk -F ' ' '{print $2}')
 
 if [ $battery_status = "discharging" ];
 then
@@ -54,21 +61,25 @@ else
    network_active="⇆"
 fi
 
-if [ $player_status = "Playing" ]
+if [ "$player_status" = "Playing" ]
 then
     song_status='▶'
-elif [ $player_status = "Paused" ]
+elif [ "$player_status" = "Paused" ]
 then
     song_status='⏸'
 else
     song_status='⏹'
 fi
 
-if [ $audio_is_muted = "true" ]
+if [ "$audio_is_muted" = "true" ]
 then
     audio_active='🔇'
 else
     audio_active='🔊'
 fi
 
-echo "🎧 $song_status $media_artist - $media_song | ⌨ $language | $network_active $interface_easyname ($ping ms) | 🏋 $loadavg_5min | $audio_active $audio_volume% | $battery_pluggedin $battery_acpi | $date_and_week 🕘 $current_time"
+load="🏋"
+phones="🎧"
+kb="⌨"
+
+echo "$phones $song_status $media_artist - $media_song | $kb $language | $network_active $network ($network_ip) | $load [ $loadavg ] | $audio_active $audio_volume% | $battery_pluggedin $battery_charge $battery_upower hrs | $date $current_time"
