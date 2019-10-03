d3ba0312c39422cb884a1b9b5a9719774f59d238
Mehmet Akif TASOVA <makiftasova@gmail.com>
2019-10-03 13:35:06 +0300

    add pre-install script
    * this script will install required packages for ArchLinux.
    * after installing packages, it will trigger dotbot's install script.

---
+++ pre-install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo pacman -S --noconfirm --needed zsh git curl terminus-font terminus-font-otb noto-fonts-{cjk,emoji,extra}
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+./install
