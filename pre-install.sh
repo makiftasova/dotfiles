7c94e1c8ea2e5f27ad8a8127afa76392f75e4a15
Mehmet Akif TASOVA <makiftasova@gmail.com>
2019-10-31 09:26:48 +0300

    update pre-install.sh for (n)vim plugins

--- pre-install.sh
+++ pre-install.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --noconfirm --needed acpi zsh git curl playerctl pamixer \
-	termite termite-terminfo terminus-font archlinux-wallpaper \
+sudo pacman -S --noconfirm --needed acpi zsh git curl neovim playerctl pamixer \
+	termite termite-terminfo terminus-font gvim archlinux-wallpaper \
 	terminus-font-otb xorg-fonts-alias wallutils \
 	noto-fonts-{cjk,emoji,extra}
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 
+# setup dotfiles
 ./install
+
+# install vim-plug and plug-ins for neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim +PlugInstall +qa!
+
+# install vim-plug and olugins for vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qa!
