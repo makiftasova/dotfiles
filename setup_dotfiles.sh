2ccb278e61673b37061b3edc06df6186c611d4fb
Mehmet Akif Tasova <makiftasova@gmail.com>
2019-11-09 15:30:31 +0300

    update setup_dotfiles.sh

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
+# install dependencies
 sudo pacman -S --noconfirm --needed acpi zsh git curl neovim playerctl pamixer \
 	termite termite-terminfo terminus-font gvim archlinux-wallpaper \
-	terminus-font-otb xorg-fonts-alias wallutils \
+	lm_sensors terminus-font-otb xorg-fonts-alias wallutils \
 	noto-fonts-{cjk,emoji,extra}
 
+# install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 
+# detect HW sensors
+yes | sudo sensors-detect
+
 # setup dotfiles
 ./install
+
+# change user shell to zsh
+chsh -s /usr/bin/zsh
 
 # install vim-plug and plug-ins for neovim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -20,3 +28,5 @@ nvim +PlugInstall +qa!
 # install vim-plug and olugins for vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qa!
+
+echo "now logout and log back in in order to use your new shell"
