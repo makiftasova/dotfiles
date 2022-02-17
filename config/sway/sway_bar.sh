397d7c899a18f594af39b53ec05957c50f149730
Mehmet Akif Tasova <makiftasova@gmail.com>
2022-02-17 21:04:19 +0300

    config/sway_bar: simplify keyboard layout command

--- config/sway/sway_bar.sh
+++ config/sway/sway_bar.sh
@@ -55,11 +55,7 @@ then
 fi
 
 # Others
-language=$(swaymsg -r -t get_inputs |\
-	awk '/1:1:AT_Translated_Set_2_keyboard/;/xkb_active_layout_name/' |\
-	grep -A1 '\b1:1:AT_Translated_Set_2_keyboard\b' |\
-	grep "xkb_active_layout_name" |\
-	awk -F '"' '{print $4}')
+language="$(swaymsg -r -t get_inputs | jq '.[] | select(.type="keyboard").xkb_active_layout_name | select( . != null)' | uniq | tr -d '"')"
 
 loadavg=$(awk -F ' ' '{print $1" "$2" "$3}' < /proc/loadavg)
 #loadavg_5min=$(awk -F ' ' '{print $2}' < /proc/loadavg)
