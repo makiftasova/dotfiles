8b9c0646f6ecabdfc1627a55f190961fae27a5f2
Mehmet Akif Tasova <makiftasova@gmail.com>
2026-08-24 20:26:55 +0300

    isolated-workspace: properly handle home diretory persistency

--- containers/isolated-workspace/run_container.sh
+++ containers/isolated-workspace/run_container.sh
@@ -7,5 +7,6 @@ podman run --detach --rm --replace \
 	--cap-add=NET_RAW \
 	--device /dev/net/tun:/dev/net/tun \
 	-v /sys/fs/cgroup:/sys/fs/cgroup:rw \
-	-v tailscale-state:/var/lib/tailscale \
+	-v isolated-workspace-home:/home/makiftasova \
+	-v isolated-workspace-tailscale:/var/lib/tailscale \
 	isolated-workspace /lib/systemd/systemd
